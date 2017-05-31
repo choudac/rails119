0535b278ed1224f6977f9d233a42fb1cffceb7b9
chouda <yujiankea@gmail.com>
2017-05-30 23:33:37 -0700

    implement my posts

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,15 @@ Rails.application.routes.draw do
   resources :groups do
     member do
       post :join
-      post :quit 
+      post :quit
     end
     resources :posts
   end
+
+ namespace :account do
+   resources :groups
+   resources :posts 
+ end
 
  root "groups#index"
 
